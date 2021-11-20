77c65a0dc9338e5deba65d7539cfdf1ca9ef4ee3
shayan2006 <54809569+shayan2006@users.noreply.github.com>
2021-11-20 19:43:46 +0530

    Add files via upload

---
+++ screens/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import db from '../config'
+import firebase from 'firebase'
+
+export default class Settings extends React.Component {
+    render(){
+        return(
+            <View>
+                
+            </View>
+        )
+    }
+}
